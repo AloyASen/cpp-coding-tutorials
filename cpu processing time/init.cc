6d598c16ca892a9fbe8024c09c2772c4ba3fb915
aloy aditya sen <aloysen951@gmail.com>
2018-01-03 16:59:04 +0530

    these are the base housekeeping jobs required for the cpp projects to keep running .. hoola

---
+++ cpu processing time/init.cc
@@ -0,0 +1,14 @@
+#include <dos.h>
+#include <time.h>
+void main(){
+    clock_t stop,start;
+    start_clock();//calling clock to start counting tick
+    for (i=0;i<=n;i++){
+        s+=a[i] *p;
+        p= p*x;
+
+    }
+    stop_clock();
+    printf("no of ticks %d", (<Start time -stop time>))
+    printf("no of seconds= %f", (start time -stop time)/CLK_TCK);
+}//main ends
---
+++ cpu processing time/modify.md
@@ -0,0 +1,8 @@
+##dated 02 01 2018
+----------------------
+
+this program is utiized for dos based systems to calculate time 
+
+for cross platform approach usee the boost libraries to get the no ticking
+
+
